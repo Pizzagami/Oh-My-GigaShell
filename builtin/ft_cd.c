886cf61893dc360831d8be0f7f4e4da7c8c2de98
Pizzagami <57038272+Pizzagami@users.noreply.github.com>
2020-06-01 15:24:23 +0200

    cd v0.1

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -1,2 +1,26 @@
-
-
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+ 
+#define BUFF_LEN        1024
+ 
+int          main(int argc, char *argv[])
+{
+  int        ret;
+  char       buff[BUFF_LEN];
+ 
+  if (argc > 1)
+    {
+      printf("Before: %s\n", getcwd(buff, BUFF_LEN -1));
+      ret = chdir(argv[1]);
+      if (ret != 0)
+        {
+          fprintf(stderr, "%s\n", strerror(errno));
+        }
+      printf("After: %s\n", getcwd(buff, BUFF_LEN -1));
+    }
+  else
+    printf("usage: %s <directory>\n", argv[0]);
+  return (0);
+}
