bf074a8db4f16809f1f94a4d726dd3bdacf220bf
billy <braimbau@student.42.fr>
2020-12-03 13:12:42 +0100

    always more norm

--- srcs/fd_gestion.c
+++ srcs/fd_gestion.c
@@ -6,7 +6,7 @@
 /*   By: braimbau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/28 15:03:37 by braimbau          #+#    #+#             */
-/*   Updated: 2020/11/26 21:15:21 by braimbau         ###   ########.fr       */
+/*   Updated: 2020/12/03 13:12:16 by braimbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_omm	init_std_fd(t_env *env, int *last_ret)
 {
 	t_omm omm;
-	
+
 	omm.env = env;
 	omm.stdin = dup(0);
 	omm.stdout = dup(1);
@@ -25,8 +25,8 @@ t_omm	init_std_fd(t_env *env, int *last_ret)
 
 void	reset_fd(t_omm omm)
 {
-		if (1)
-			dup2(omm.stdout, 1);
-		if (1)
-			dup2(omm.stdin, 0);
+	if (1)
+		dup2(omm.stdout, 1);
+	if (1)
+		dup2(omm.stdin, 0);
 }
