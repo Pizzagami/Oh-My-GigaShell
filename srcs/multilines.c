c328fa68ea96edb29df3a25884fa4e42eaa135f5
billy <braimbau@student.42.fr>
2021-01-21 14:44:08 +0100

    fix lignes vides histo

--- srcs/multilines.c
+++ srcs/multilines.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   multilines.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: selgrabl <selgrabl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: braimbau <braimbau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 10:49:12 by braimbau          #+#    #+#             */
-/*   Updated: 2021/01/20 15:53:01 by selgrabl         ###   ########.fr       */
+/*   Updated: 2021/01/21 14:42:20 by braimbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ int	multilines(t_hist *hist, t_env *env, int *last_ret, t_multi *multi)
 		free(multi->str);
 		multi->type = 0;
 	}
-	if (multi->x == 0)
-		hist->tab[0] = (multi->type) ?
-	ft_strjoin_sep(multi->str, "", '\n') : "";
-	ret = persecutor(hist, env, last_ret);
+	if (multi->x == 0 && multi->type)
+		hist->tab[0] = ft_strjoin_sep(multi->str, "", '\n');
+	if (multi->type || multi->x)
+		ret = persecutor(hist, env, last_ret);
 	if (ret == 1 || ret == 2 || ret == 7 || ret == 12 || ret == 13)
 	{
 		multi->str = ft_strdup(hist->tab[0]);
