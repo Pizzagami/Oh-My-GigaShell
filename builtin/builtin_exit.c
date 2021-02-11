6fd1f3cb9c3ace8b75c5dff308a613af40ffd911
pizzagami <selgrabl@student.42.fr>
2021-02-11 20:51:01 +0100

    exit

--- builtin/builtin_exit.c
+++ builtin/builtin_exit.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: braimbau <braimbau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pizzagami <pizzagami@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 10:23:00 by selgrabl          #+#    #+#             */
-/*   Updated: 2021/02/10 15:33:56 by braimbau         ###   ########.fr       */
+/*   Updated: 2021/02/11 20:50:28 by pizzagami        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+void numerror(char *str)
+{
+	ft_putstr_fd("exit \nbash: exit: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(" numeric argument required\n", 2);
+}
+
+void whilerror(char **str, int *j, t_env *env, int *ret)
+{
+	if (ft_isdigit(str[0][*j]))
+		*j++;
+	else
+		{
+			numerror(*str);
+			free_env(env);
+			exit (255);
+		}
+	ret = (ret >= 0) ? ft_atoi(str[0]) % 255: 256 - (ft_atoi(str[0]) % 255) ;
+}
 
 void	builtin_exit(t_env *env, char **str)
 {
@@ -22,11 +41,7 @@ void	builtin_exit(t_env *env, char **str)
 	if (!(str && str[0]))
 		ft_putstr_fd("exit\n", 2);
 	else if (ft_isalpha(str[0][j]))
-		{
-			ft_putstr_fd("exit \nbash: exit: ", 2);
-			ft_putstr_fd(*str, 2);
-			ft_putstr_fd(" numeric argument required\n", 2);
-		}
+		numerror(*str);
 	else
 	{
 		if (str[0][0] == '-')
@@ -35,23 +50,9 @@ void	builtin_exit(t_env *env, char **str)
 			j++;
 		}
 		while (str[0][j])
-		{
-			if (ft_isdigit(str[0][j]))
-				j++;
-			else
-				{
-					ft_putstr_fd("exit \nbash: exit: ", 2);
-					ft_putstr_fd(*str, 2);
-					ft_putstr_fd(" numeric argument required\n", 2);
-					exit (255);
-				}
-			ret *= ft_atoi(str[0]);
-		}
+			whilerror(str, &j, env, &ret);
 		if (str[1] && str[1][0])
-		{
-			ft_putstr_fd("exit: too many arguments", 2);
-			return ;
-		}
+			return (ft_putstr_fd("exit: too many arguments", 2));
 	}
 	free_env(env);
 	exit(ret);
